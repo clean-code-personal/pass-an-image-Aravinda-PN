11e8efe539bbf6e4d55e05442546f5728071c943
PN, Aravinda <Aravinda.Nagendra@philips.com>
2024-04-10 16:19:45 +0530

    fix stack overflow

--- brightener.h
+++ brightener.h
@@ -4,7 +4,7 @@
 struct Image {
 	int rows;
 	int columns;
-	uint8_t pixels[1024 * 1024]; // max 1k x 1k image
+	uint8_t* pixels = new uint8_t[1024 * 1024]; // max 1k x 1k image
 };
 
 class ImageBrightener {
